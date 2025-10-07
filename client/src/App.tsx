2b76ca0ba3d9753d505cc4d8cb3195166d3003f6
Tancredi Russo <tancredirusso@MacBook-Air-di-Tancredi.fritz.box>
2025-10-07 22:19:05 +0200

    fix(router): imposta base '/ciclo-del-carbonio' per wouter su GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -22,7 +22,7 @@ import NotFound from "@/pages/not-found";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 
-function Router() {
+function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -45,13 +45,15 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <div className="min-h-screen flex flex-col bg-background">
-          <Navigation />
-          <main className="flex-1">
-            <Router />
-          </main>
-          <Footer />
-        </div>
+        <WouterRouter base="/ciclo-del-carbonio">
+          <div className="min-h-screen flex flex-col bg-background">
+            <Navigation />
+            <main className="flex-1">
+              <AppRoutes />
+            </main>
+            <Footer />
+          </div>
+        </WouterRouter>
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
